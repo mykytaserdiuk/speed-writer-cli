d4492b2b502a6aa97063e5ed2023ec13b8208b4e
Mykyta <hiban.com@gmail.com>
2024-12-13 19:33:45 +0100

    Base dirty cli

---
+++ key-checker.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/jroimartin/gocui"
+
+var (
+	NotAllowedKeys = []gocui.Key{gocui.KeyArrowDown, gocui.KeyArrowUp, gocui.KeyArrowLeft, gocui.KeyArrowRight, gocui.KeyDelete}
+)
+
+func CheckAllowingKey(key gocui.Key) bool {
+	for _, k := range NotAllowedKeys {
+		if k == key {
+			return false
+		}
+	}
+	return true
+}
